9ae99f434bfcbac75363cc5238dc6db635396669
Sean Murphy <sean@iamseanmurphy.com>
2026-05-26 13:21:58 +0000

    6.30.11

--- skt-templates/vendor/websites/templates-directory/class-page-templates-directory.php
+++ skt-templates/vendor/websites/templates-directory/class-page-templates-directory.php
@@ -397,6 +397,15 @@ if ( ! class_exists( '\SktThemes\PageTemplatesDirectory' ) ) {
 				'required_plugins' => array( 'elementor' => array( 'title' => __( 'Elementor Page Builder', 'skt-templates' ) ) ),
 			);
 			$templates_list = array(
+				'skt-copywriter'	=> array(
+					'title'       => __( 'SKT Copywriter', 'skt-templates' ),
+					'description' => __( 'It downloads from our website sktthemes.org, once you do it you will get the exact preview like shown in the demo. Steps after downloading the theme: Upload it via appearance>themes>add new>upload theme zip file and activate the theme.', 'skt-templates' ),
+					'theme_url'   => esc_url('#'),
+					'demo_url'    => esc_url('https://demosktthemes.com/free/skt-copywriter/'),
+					'screenshot'  => esc_url('https://demosktthemes.com/free/skt-copywriter/skt-copywriter.jpg'),
+					'import_file' => esc_url('https://demosktthemes.com/free/skt-copywriter/skt-copywriter.json'),
+					'keywords'    => __( ' content writer, advertising writer, marketing writer, creative writer, copy editor, content creator, brand storyteller, ad copywriter, wordsmith, communications writer, copy writer, Copy Writer' ),
+				),
 				'skt-plastic-surgery'	=> array(
 					'title'       => __( 'SKT Plastic Surgery', 'skt-templates' ),
 					'description' => __( 'It downloads from our website sktthemes.org, once you do it you will get the exact preview like shown in the demo. Steps after downloading the theme: Upload it via appearance>themes>add new>upload theme zip file and activate the theme.', 'skt-templates' ),
